e18b99644c888e5c9df0fbf1c34ad7006b3e0658
frederik-hoeft <43813142+frederik-hoeft@users.noreply.github.com>
2023-08-08 18:29:13 +0200

    revert fancy syntax

--- src/stopwatch_controller.vhd
+++ src/stopwatch_controller.vhd
@@ -38,36 +38,53 @@ begin
             -- update local snapshots of btn_toggle and btn_reset
             _toggle <= btn_toggle;
             _reset <= btn_reset;
-            -- state transition logic, basically a C#-style switch-case on _state
-            -- with switch expressions for next state
+            -- whatever VHDL version we're using doesn't seem to support VHDL-2008 syntax...
+            -- so when-else (kinda like C# switch expressions) doesn't seem to work :P
             case _state is
-                -- 000: "zero", 0- => 000, 1- => 001
-                when "000" => _state <= 
-                    "001" when (_toggle = '1') else 
-                    "000";
-                -- 001: "start", 00 => 010, 10|11|01 => 001
-                when "001" => _state <= 
-                    "010" when (_toggle = '0') and (_reset = '0') else 
-                    "001";
-                -- 010: "running", 0- => 010, 1- => 011
-                when "010" => _state <= 
-                    "011" when (_toggle = '1') else 
-                    "010";
-                -- 011: "stop", 00 => 100, 10|11|01 => 011
-                when "011" => _state <= 
-                    "100" when (_toggle = '0') and (_reset = '0') else 
-                    "011";
-                -- 100: "stopped", 00 => 100, 01 => 101, 1- => 001
-                when "100" => _state <= 
-                    "001" when (_toggle = '1') else 
-                    "101" when (_reset = '1') else 
-                    "100";
-                -- 101: "reset", 00 => 000, 01|11|10 => 101
-                when "101" => _state <= 
-                    "000" when (_reset = '0') and (_toggle = '0') else 
-                    "101";
-                when others =>
-                    report "Invalid state" severity failure;
+                -- 000: "zero" <=> 00, 0- => 000, 1- => 001
+                when "000" =>
+                    if (_toggle = '1') then
+                        _state <= "001";
+                    else
+                        _state <= "000";
+                    end if;
+                -- 001: "start" <=> 10, 00 => 010, 10|11|01 => 001
+                when "001" =>
+                    if (_toggle = '0') and (_reset = '0') then
+                        _state <= "010";
+                    else
+                        _state <= "001";
+                    end if;
+                -- 010: "running" <=> 10, 0- => 010, 1- => 011
+                when "010" =>
+                    if (_toggle = '1') then
+                        _state <= "011";
+                    else
+                        _state <= "010";
+                    end if;
+                -- 011: "stop" <=> 00, 00 => 100, 10|11|01 => 011
+                when "011" =>
+                    if (_toggle = '0') and (_reset = '0') then
+                        _state <= "100";
+                    else
+                        _state <= "011";
+                    end if;
+                -- 100: "stopped" <=> 00, 00 => 100, 01 => 101, 1- => 001
+                when "100" =>
+                    if (_toggle = '1') then
+                        _state <= "001";
+                    elsif (_reset = '1') then
+                        _state <= "101";
+                    else
+                        _state <= "100";
+                    end if;
+                -- 101: "reset" <=> 01, 00 => 000, 01|11|10 => 101
+                when "101" =>
+                    if (_reset = '0') and (_toggle = '0') then
+                        _state <= "000";
+                    else
+                        _state <= "101";
+                    end if;
             end case;
         end if;
     end process;
