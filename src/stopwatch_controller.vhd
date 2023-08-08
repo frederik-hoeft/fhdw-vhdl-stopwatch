24bf5b7f6603160bb458f7586861dcbab032e33b
frederik-hoeft <43813142+frederik-hoeft@users.noreply.github.com>
2023-08-08 17:09:12 +0200

    define transition table

--- src/stopwatch_controller.vhd
+++ src/stopwatch_controller.vhd
@@ -30,15 +30,33 @@ use IEEE.STD_LOGIC_1164.ALL;
 --use UNISIM.VComponents.all;
 
 entity stopwatch_controller is
-    Port ( btn_toggle : in  STD_LOGIC;
-           btn_reset : in  STD_LOGIC;
-           ctr_clear : out  STD_LOGIC;
-           ctr_enable : out  STD_LOGIC);
+    Port ( clk : in  STD_LOGIC; -- clock
+           btn_toggle : in  STD_LOGIC; -- start/stop button for stopwatch, '1'-active
+           btn_reset : in  STD_LOGIC; -- reset button for stopwatch, '0'-active
+           ctr_clear : out  STD_LOGIC; -- '1'-active reset for 16 bit couter
+           ctr_enable : out  STD_LOGIC); -- '1'-active enable signal for counter
 end stopwatch_controller;
 
 architecture Behavioral of stopwatch_controller is
 
+-- "-" <==> "don't care", "|" <==> "OR"
+-- STATE <=> OUTPUT (ctr_enable, ctr_clear), TRANSITION 1 (btn_toggle, btn_reset) => STATE, TRANSITION 2 ...
+-- 000: "zero" <=> 00, 0- => 000, 1- => 001
+-- 001: "start" <=> 10, 00 => 010, 10|11|01 => 001
+-- 010: "running" <=> 10, 0- => 010, 1- => 011
+-- 011: "stop" <=> 00, 00 => 100, 10|11|01 => 011
+-- 100: "stopped" <=> 00, 00 => 100, 01 => 101, 1- => 001
+-- 101: "reset" <=> 01, 00 => 000, 01|11|10 => 101
+signal _state: std_logic_vector(2 downto 0);
+
 begin
+
+    process (clk)
+    begin
+        if rising_edge(clk) then
+
+        end if;
+    end process;
 
 
 end Behavioral;
